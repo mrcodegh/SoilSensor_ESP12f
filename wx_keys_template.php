8a10832b459fb6b5159d4851a4297ce3b5d7ea83
Mike Russell <miker_nomon@gmx.com>
2019-01-20 14:52:58 -0600

    Push latest quailvalleywx code

---
+++ wx_keys_template.php
@@ -0,0 +1,30 @@
+<?php
+
+//$servername = "localhost";
+$servername = "127.0.0.1";
+$username = "";
+$password = "";
+
+$wu_id = "";
+$wu_pwd = "";
+
+$wc_id = "";
+$wc_key = "";
+
+$pws_id = "";
+$pws_pwd = "";
+
+$owm_id = "";
+
+$cwop_id = "";
+$cwop_pwd = "";
+
+$blitzortung_user = "";
+$blitzortung_pwd = "";
+
+$bing_map_key = "";
+$opencage_key = "";
+$darksky_key = "";
+$synoptic_token = "";
+
+?>
